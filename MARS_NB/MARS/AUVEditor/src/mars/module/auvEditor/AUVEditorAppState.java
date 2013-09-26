90ec11a765a42a78245574be5a673a5ff76b7c2a
Alexander Bigerl <bigerl@informatik.uni-luebeck.de>
2013-09-26 14:52:29 +0200

    inzial commit for AUV Editor

---
+++ MARS_NB/MARS/AUVEditor/src/mars/module/auvEditor/AUVEditorAppState.java
@@ -0,0 +1,452 @@
+package mars.module.auvEditor;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.asset.plugins.FileLocator;
+import com.jme3.collision.CollisionResult;
+import com.jme3.collision.CollisionResults;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.MouseInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.input.controls.MouseAxisTrigger;
+import com.jme3.input.controls.MouseButtonTrigger;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState.BlendMode;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.math.Ray;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.debug.Grid;
+import com.jme3.scene.shape.Line;
+import mars.states.AppStateExtension;
+
+/**
+ * Appstate for pointing out attatchments on a AUV
+ */
+public class AUVEditorAppState extends AbstractAppState implements AppStateExtension{
+
+    private Node rotationOrbNode;
+
+    public enum Phase {
+
+	POS, ROT, TYPE;
+    }
+    private Phase phase = Phase.POS;
+    /**
+     * cam of parent SimpleApplication
+     */
+    private Camera cam;
+    /**
+     * parent SimpleApplication
+     */
+    private SimpleApplication app;
+    /**
+     * rootNode of parent SimpleApplication
+     */
+    private Node rootNode = new Node("AUVEditor Root Node");
+    /**
+     * all spatials which can be selected by mouse are under this node
+     */
+    private Node selectables;
+    /**
+     * assetManager of parent SimpleApplication
+     */
+    private AssetManager assetManager;
+    /**
+     * inputManager of parent SimpleApplication
+     */
+    private InputManager inputManager;
+    /**
+     * Node containing coordinate axes
+     */
+    private Node coordinateAxesNode;
+    /**
+     * speed for moving actions
+     */
+    private float speed = 1f;
+    /**
+     * Node containing the AUV
+     */
+    private Node auvNode;
+    /**
+     * memorizes the geometry to which the coordinate axes are attached by
+     * CoordinateAxesControl
+     */
+    private Geometry currentCoordinateAxesControlSelected;
+
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+	super.initialize(stateManager, app);
+	this.app = (SimpleApplication) app;
+	this.inputManager = this.app.getInputManager();
+	this.assetManager = this.app.getAssetManager();
+
+	//assetManager.registerLocator("./assets", FileLocator.class);
+
+	selectables = new Node("selectables");
+	/*rootNode.attachChild(selectables);
+
+	rootNode.attachChild(getLineCross());
+	rootNode.attachChild(getWireFrameCross());*/
+
+//	initKeys();
+//	initCoordinateAxes();
+//	initRationOrb();
+//	initAUVNode();
+//	loadAUV("Models/Cube.obj");
+//	loadAUV("Models/Cube.obj");
+//	loadAUV("Models/Cube.obj");
+    }
+
+    @Override
+    public Node getRootNode() {
+	return rootNode;
+    }
+    
+    public Camera getCamera() {
+	return cam;
+    }
+
+    @Override
+    public void setCamera(Camera cam) {
+        this.cam = cam;
+    }
+
+    @Override
+    public void update(float tpf) {
+	super.update(tpf);
+    }
+
+    private void initKeys() {
+	System.out.println(SimpleApplication.INPUT_MAPPING_MEMORY);
+	inputManager.deleteMapping("FLYCAM_ZoomIn");
+	inputManager.deleteMapping("FLYCAM_ZoomOut");
+	// Keymapping for moving the coordinate axes
+	inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_U));
+	inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_M));
+	inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_J));
+	inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_L));
+	inputManager.addMapping("Forward", new KeyTrigger(KeyInput.KEY_I));
+	inputManager.addMapping("Backward", new KeyTrigger(KeyInput.KEY_K));
+	inputManager.addMapping("SelectObject", new MouseButtonTrigger(MouseInput.BUTTON_RIGHT));
+	inputManager.addMapping("SelectManipulator", new MouseButtonTrigger(MouseInput.BUTTON_MIDDLE));
+	inputManager.addMapping("Scale Up", new MouseAxisTrigger(MouseInput.AXIS_WHEEL, false));
+	inputManager.addMapping("Scale Down", new MouseAxisTrigger(MouseInput.AXIS_WHEEL, true));
+	// Mousebuttonmapping for selecting selectables
+	inputManager.addListener(actionListener, "SelectObject");
+
+    }
+    /**
+     * Action listener to select selectables like AUVs and attachments.
+     */
+    ActionListener actionListener = new ActionListener() {
+	@Override
+	public void onAction(String name, boolean isPressed, float tpf) {
+	    if (name.equals("SelectObject") && !isPressed) {
+		CollisionResult closestCollision = getClosestCollisionToMouseRay(selectables);
+
+		// deselect the current object
+		if (currentCoordinateAxesControlSelected != null) {
+		    currentCoordinateAxesControlSelected.getControl(CoordinateAxesControl.class).setEnabled(false);
+		    currentCoordinateAxesControlSelected = null;
+		}
+
+		// select a new object
+		if (closestCollision != null) {
+		    closestCollision.getGeometry().getControl(CoordinateAxesControl.class).setEnabled(true);
+		    currentCoordinateAxesControlSelected = closestCollision.getGeometry();
+		}
+	    }
+	}
+    };
+
+    /**
+     * Gets the closest collsion to the mouse ray
+     *
+     * @param allowedTargets only intersections with this node and its childs
+     * are returned
+     * @return the ClosestCollison object if any exists, else null
+     */
+    public CollisionResult getClosestCollisionToMouseRay(Node... allowedTargets) {
+	// get vector directed to the clicked object
+	Vector2f click2d = inputManager.getCursorPosition();
+	Vector3f click3d = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 0f).clone();
+	Vector3f dir = cam.getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(click3d).normalizeLocal();
+	// get first collision for all allowedTargets and find the closest
+	CollisionResult closestCollision = null;
+	for (Node target : allowedTargets) {
+	    CollisionResults results = new CollisionResults();
+	    Ray ray = new Ray(click3d, dir);
+	    target.collideWith(ray, results);
+	    if (closestCollision != null && results.getClosestCollision() != null && results.getClosestCollision().getDistance() < closestCollision.getDistance()) {
+		closestCollision = results.getClosestCollision();
+	    }
+	    if (closestCollision == null) {
+		closestCollision = results.getClosestCollision();
+	    }
+	}
+	return closestCollision;
+    }
+
+    /**
+     * inzializes coordinate axes
+     */
+    private void initCoordinateAxes() {
+	coordinateAxesNode = new Node("Coordinate Axes");
+	coordinateAxesNode.attachChild(getArrowNode(7, "x Axis", ColorRGBA.Red, 'x'));
+	coordinateAxesNode.attachChild(getArrowNode(7, "y Axis", ColorRGBA.Green, 'y'));
+	coordinateAxesNode.attachChild(getArrowNode(7, "z Axis", ColorRGBA.Blue, 'z'));
+	rootNode.attachChild(coordinateAxesNode);
+	coordinateAxesNode.setCullHint(Spatial.CullHint.Always);
+    }
+
+    /**
+     * inzializes rotation orb
+     */
+    private void initRationOrb() {
+	rotationOrbNode = new Node("Rotation Orb");
+	rotationOrbNode.attachChild(getTorusNode("x Torus", ColorRGBA.Red, 'x'));
+	rotationOrbNode.attachChild(getTorusNode("y Torus", ColorRGBA.Green, 'y'));
+	rotationOrbNode.attachChild(getTorusNode("z Torus", ColorRGBA.Blue, 'z'));
+	rootNode.attachChild(rotationOrbNode);
+	rotationOrbNode.setCullHint(Spatial.CullHint.Always);
+    }
+
+    /**
+     * returns a node containing a arrow of given color and length. Minimum
+     * length is 1.
+     *
+     * @param length length of the arrow
+     * @param name name of the node
+     * @param color color of the arrow
+     * @param axis 'x', 'y' or 'z' to define orientation
+     * @return a node containing a arrow consisting of a cylinder and a cone
+     */
+    private Node getArrowNode(float length, String name, ColorRGBA color, char axis) {
+	// check if length is at least 1
+	if (length < 1) {
+	    length = 1;
+	}
+	// prepare colored material
+	Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+	mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+	color.a = 0.7f;
+	mat.setColor("Color", color);
+
+	// init return node
+	Node node = new Node(name);
+
+	// build and scale cylinder
+	Spatial cylinder = assetManager.loadModel("Models/Cylinder.obj");
+	cylinder.scale(1, length - 1, 1);
+	cylinder.setLocalTranslation(Vector3f.ZERO);
+	cylinder.setMaterial(mat);
+	node.attachChild(cylinder);
+
+	// build tip
+	Spatial cone = assetManager.loadModel("Models/Cone.obj");
+	cone.setLocalTranslation(0, length - 1, 0);
+	cone.setMaterial(mat);
+	node.attachChild(cone);
+
+	// set rotation
+	switch (axis) {
+	    case 'x':
+		node.rotate(0, 0, -90 * FastMath.DEG_TO_RAD);
+		break;
+	    case 'y':
+		break;
+	    case 'z':
+		node.rotate(90 * FastMath.DEG_TO_RAD, 0, 0);
+		break;
+	}
+	return node;
+    }
+
+    /**
+     *
+     * @param name name of the node
+     * @param color color of the arrow
+     * @param axis 'x', 'y' or 'z' to define orientation
+     * @return a node containing a sphere
+     */
+    private Node getTorusNode(String name, ColorRGBA color, char axis) {
+	// prepare colored material
+	Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+	mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+	color.a = 0.7f;
+	mat.setColor("Color", color);
+
+	// init return node
+	Node node = new Node(name);
+
+	// build and scale cylinder
+	Spatial torus = assetManager.loadModel("Models/Torus.obj");
+	torus.setLocalTranslation(Vector3f.ZERO);
+	torus.setMaterial(mat);
+	node.attachChild(torus);
+	// set rotation
+	switch (axis) {
+	    case 'x':
+		node.rotate(0, 0, 0);
+		break;
+	    case 'y':
+		node.rotate(0, 0, 90 * FastMath.DEG_TO_RAD);
+		break;
+	    case 'z':
+		node.rotate(0, 90 * FastMath.DEG_TO_RAD, 0);
+		break;
+	}
+	return node;
+    }
+
+    /**
+     * Make a node containing a line cross node
+     *
+     * @return a node containing a cross
+     */
+    private Node getLineCross() {
+	Node lineCross = new Node("Line Cross");
+	lineCross.attachChild(getLine(ColorRGBA.Red, 'x'));
+	lineCross.attachChild(getLine(ColorRGBA.Green, 'y'));
+	lineCross.attachChild(getLine(ColorRGBA.Blue, 'z'));
+
+	return lineCross;
+    }
+
+    private Node getWireFrameCross() {
+	Node wireFrameCross = new Node("Wireframe Cross");
+	wireFrameCross.attachChild(getWireFrame("x Plain", 'x'));
+	wireFrameCross.attachChild(getWireFrame("y Plain", 'y'));
+	wireFrameCross.attachChild(getWireFrame("z Plain", 'z'));
+	return wireFrameCross;
+    }
+
+    /**
+     * Makes a node containing a line in the axis direction
+     *
+     * @param color Color of the line
+     * @param axis 'x'(default), 'y' or 'z'
+     * @return
+     */
+    private Node getLine(ColorRGBA color, char axis) {
+	// set direction and name
+	Vector3f direction;
+	String name;
+	float length = 1000000;
+	switch (axis) {
+	    default:
+	    case 'x':
+		direction = new Vector3f(length / 2, 0, 0);
+		name = "x Axis";
+		break;
+	    case 'y':
+		direction = new Vector3f(0, length / 2, 0);
+		name = "y Axis";
+		break;
+	    case 'z':
+		direction = new Vector3f(0, 0, length / 2);
+		name = "z Axis";
+		break;
+	}
+
+	Node node = new Node(name);
+	Line line = new Line(direction, direction.clone().mult(-1));
+	Geometry lineGeo = new Geometry(name, line);
+
+	// set material
+	Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+	mat.setColor("Color", color);
+	lineGeo.setMaterial(mat);
+	// attach node
+	node.attachChild(lineGeo);
+	return node;
+    }
+
+    /**
+     * get a node containing wireframe in the plain normal to axis
+     *
+     * @param name nodes name
+     * @param axis 'x'(default), 'y' or 'z'
+     * @return
+     */
+    private Node getWireFrame(String name, char axis) {
+	Geometry frame = new Geometry("Wireframe Grid", new Grid(10, 10, 1f));
+
+	Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+	mat.getAdditionalRenderState().setWireframe(true);
+	mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+	mat.setColor("Color", new ColorRGBA(1, 1, 1, 0.5f));
+	frame.setMaterial(mat);
+
+	// init return node
+	Node node = new Node(name);
+	node.attachChild(frame);
+	frame.center().move(Vector3f.ZERO);
+
+	// set direction and name
+	switch (axis) {
+	    default:
+	    case 'x':
+		node.rotate(0, 0, 90 * FastMath.DEG_TO_RAD);
+		break;
+	    case 'y':
+		node.rotate(0, 0, 0);
+		break;
+	    case 'z':
+		node.rotate(90 * FastMath.DEG_TO_RAD, 0, 0);
+		break;
+	}
+	return node;
+    }
+
+    /**
+     * inizializes "AUV Node"
+     */
+    private void initAUVNode() {
+	auvNode = new Node("AUV Node");
+	selectables.attachChild(auvNode);
+    }
+
+    /**
+     * loads a AUV and attaches it to the "AUV Node"
+     *
+     * @param path path relativ to "./assets"
+     * @return true: was loaded successful<br/>
+     * not found or not able to load the file
+     */
+    public boolean loadAUV(String path) {
+	try {
+	    // load
+	    Spatial auv = assetManager.loadModel(path);
+
+	    // set material
+	    Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+	    mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+	    ColorRGBA color = ColorRGBA.Orange;
+	    color.a = 0.3f;
+	    mat.setColor("Color", color);
+	    auv.setMaterial(mat);
+	    // put it at point of origin
+	    auv.setLocalTranslation(Vector3f.ZERO);
+	    // attach CoordinateAxesControl
+	    auv.addControl(new CoordinateAxesControl(coordinateAxesNode, rotationOrbNode, speed, inputManager, auv, this));
+	    // attach it at "AUV Node"
+	    auvNode.attachChild(auv);
+	    return true;
+	} catch (Exception e) {
+	    return false;
+	}
+    }
+}
---
+++ MARS_NB/MARS/AUVEditor/src/mars/module/auvEditor/CoordinateAxesControl.java
@@ -0,0 +1,324 @@
+/*
+ */
+package mars.module.auvEditor;
+
+import com.jme3.collision.CollisionResult;
+import com.jme3.input.InputManager;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.AnalogListener;
+import com.jme3.math.FastMath;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.AbstractControl;
+
+/**
+ * Control class for Objects that should be moved by "ijklum"-keys with a
+ * visible coordinate system.<br/>
+ * There must always be maximum one enabled instance of the class!
+ */
+public class CoordinateAxesControl extends AbstractControl {
+
+    /**
+     * the node containing the coordinate system, set by the constructor
+     */
+    private Node coordinateAxesNode;
+    /**
+     * movement speed, set by the constructor
+     */
+    private float speed;
+    /**
+     * inputManager of simpleApplication
+     */
+    private InputManager inputManager;
+    /**
+     * selected axis to drag the object
+     */
+    private Node currentlySelectedAxis;
+    /**
+     * position of the mouse projected on the axis
+     */
+    Vector3f mouseOnAxisPosition;
+    /**
+     * parent appstate
+     */
+    private AUVEditorAppState appState;
+    private Node rotationOrbNode;
+    private Node currentlySelectedOrb;
+    private Vector3f rotationVector;
+
+    public CoordinateAxesControl() {
+	super();
+    }
+
+    /**
+     * use only this constructor. The CoordinateAxesControl is disabled after
+     * construction.
+     *
+     * @param coordinateAxesNode node containing a coordinate system
+     * @param rotationOrbNode node containing a rotion orb
+     * @param speed movement speed
+     * @param inputManager the application's input manager
+     * @param spatial the spatial the controller will be attached to
+     */
+    public CoordinateAxesControl(Node coordinateAxesNode, Node rotationOrbNode, float speed, InputManager inputManager, Spatial spatial, AUVEditorAppState appState) {
+	super();
+	setSpatial(spatial);
+	this.coordinateAxesNode = coordinateAxesNode;
+	this.rotationOrbNode = rotationOrbNode;
+	this.speed = speed;
+	this.inputManager = inputManager;
+	this.appState = appState;
+	initKeys();
+	super.setEnabled(false);
+
+    }
+
+    private void initKeys() {
+	inputManager.addListener(analogListener, "Up", "Down", "Left", "Right", "Forward", "Backward", "SelectManipulator", "Scale Up", "Scale Down");
+	inputManager.addListener(actionListener, "SelectManipulator");
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+	super.setEnabled(enabled);
+	if (enabled) {
+	    // move the coordinate axes and rotation orb to the selected AUV
+	    coordinateAxesNode.setLocalTranslation(spatial.getWorldTranslation());
+	    rotationOrbNode.setLocalTranslation(spatial.getWorldTranslation());
+
+	    // show them
+	    coordinateAxesNode.setCullHint(Spatial.CullHint.Inherit);
+	    rotationOrbNode.setCullHint(Spatial.CullHint.Inherit);
+	} else {
+	    // hide them
+	    coordinateAxesNode.setCullHint(Spatial.CullHint.Always);
+	    rotationOrbNode.setCullHint(Spatial.CullHint.Always);
+	}
+    }
+    /**
+     * starts and stops the draging by coordinate axes
+     */
+    ActionListener actionListener = new ActionListener() {
+	@Override
+	public void onAction(String name, boolean isPressed, float tpf) {
+	    if (isEnabled()) {
+
+		/*
+		 * check if SelectAxis is pressed and this wasn't called before (currentlySelectedAxis == null) on this press period
+		 */
+		if (name.equals("SelectManipulator") && isPressed && currentlySelectedAxis == null && currentlySelectedOrb == null) {
+		    CollisionResult closestCollision = appState.getClosestCollisionToMouseRay(coordinateAxesNode, rotationOrbNode);
+		    // set the current axis
+		    if (closestCollision != null) {
+			Node temp = closestCollision.getGeometry().getParent().getParent();
+			switch (temp.getName()) {
+			    case "Coordinate Axes":
+				currentlySelectedAxis = closestCollision.getGeometry().getParent();
+				break;
+			    case "Rotation Orb":
+				currentlySelectedOrb = closestCollision.getGeometry().getParent();
+				break;
+			}
+		    }
+		}
+		/*
+		 * check if draging was finished
+		 */
+		if (name.equals("SelectManipulator") && !isPressed) {
+		    // deselect the current object
+		    currentlySelectedAxis = null;
+		    currentlySelectedOrb = null;
+		    mouseOnAxisPosition = null;
+		    rotationVector = null;
+		    rotationOrbNode.setLocalRotation(Matrix3f.ZERO);
+		}
+	    }
+	}
+    };
+    /**
+     * Analog listener to move spatial and coordinate axes and rotation orb.
+     * Works only when controller is enabled.<br/>
+     * The analogListener is only listening for key strokes on "ijklum" and for
+     * dragging by the middle mouse button.
+     */
+    AnalogListener analogListener = new AnalogListener() {
+	@Override
+	public void onAnalog(String name, float value, float tpf) {
+	    if (isEnabled()) {
+		Vector3f moveVector = new Vector3f(0, 0, 0);
+		Quaternion rotationQuaternion = new Quaternion();
+		/*
+		 * scale
+		 */
+		if (name.equals("Scale Up")) {
+		    spatial.scale(1.1f, 1.1f, 1.1f);
+
+		}
+		if (name.equals("Scale Down")) {
+		    spatial.scale(10f / 11);
+		}
+
+		/*
+		 * rotate by drag'n'drop
+		 */
+		if (name.equals("SelectManipulator") && currentlySelectedOrb != null) {
+		    // get plain
+		    Vector3f plainBasis = currentlySelectedOrb.getWorldTranslation();
+		    Vector3f plainNormal;
+		    switch (currentlySelectedOrb.getName()) {
+			case "x Torus":
+			    plainNormal = Vector3f.UNIT_X;
+			    break;
+			case "y Torus":
+			    plainNormal = Vector3f.UNIT_Y;
+			    break;
+			default:
+			case "z Torus":
+			    plainNormal = Vector3f.UNIT_Z;
+			    break;
+		    }
+
+		    // get line
+		    Vector2f click2d = inputManager.getCursorPosition();
+		    Vector3f lineBasis = appState.getCamera().getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 0f).clone();
+		    Vector3f lineDirection = appState.getCamera().getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(lineBasis).normalizeLocal();
+
+		    // get new plain projected mouse position
+		    Vector3f currentValue = getIntersectionWithPlane(plainBasis, plainNormal, lineBasis, lineDirection, "");
+
+		    Vector3f v_ = currentValue.subtract(plainBasis);
+		    // current value projected on the unit circle
+		    Vector3f v_n = v_.divide(v_.length());
+		    if (rotationVector != null) {
+			// last value projected on the unit circle
+			Vector3f k_n = rotationVector;
+			// rotate k_n back 45° to avoid the negativ angles (angleBetween() return only positiv values
+			Matrix3f rotateMinusQuarterPi = new Matrix3f();
+			rotateMinusQuarterPi.fromAngleAxis(-FastMath.QUARTER_PI, plainNormal);
+			Vector3f k_n_MinusQuarterPi = rotateMinusQuarterPi.mult(k_n);
+
+			rotationQuaternion.fromAngleAxis(k_n_MinusQuarterPi.angleBetween(v_n) - FastMath.QUARTER_PI, plainNormal);
+		    }
+		    rotationVector = v_n;
+		}
+
+		/*
+		 * move by drag'n'drop
+		 */
+		if (name.equals("SelectManipulator") && currentlySelectedAxis != null) {
+		    // get plain
+		    Vector3f plainBasis = coordinateAxesNode.getWorldTranslation();
+		    Vector3f plainNormal;
+		    switch (currentlySelectedAxis.getName()) {
+			case "x Axis":
+			    plainNormal = Vector3f.UNIT_Y;
+			    break;
+			case "y Axis":
+			    plainNormal = Vector3f.UNIT_Z;
+			    break;
+			default:
+			case "z Axis":
+			    plainNormal = Vector3f.UNIT_X;
+			    break;
+		    }
+
+		    // get line
+		    Vector2f click2d = inputManager.getCursorPosition();
+		    Vector3f lineBasis = appState.getCamera().getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 0f).clone();
+		    Vector3f lineDirection = appState.getCamera().getWorldCoordinates(new Vector2f(click2d.x, click2d.y), 1f).subtractLocal(lineBasis).normalizeLocal();
+
+		    // get new axis projected mouse position
+		    Vector3f currentValue = getIntersectionWithPlane(plainBasis, plainNormal, lineBasis, lineDirection, currentlySelectedAxis.getName());
+
+		    // move by diffrence to previous value
+		    if (mouseOnAxisPosition != null) {
+			moveVector = currentValue.subtract(mouseOnAxisPosition);
+		    }
+
+		    // save current value for next call
+		    mouseOnAxisPosition = currentValue;
+		}
+
+		/*
+		 * move by keys
+		 */
+		switch (name) {
+		    case "Up":
+			moveVector = new Vector3f(0, speed * value, 0);
+			break;
+		    case "Down":
+			moveVector = new Vector3f(0, speed * value * (-1), 0);
+			break;
+		    case "Left":
+			moveVector = new Vector3f(speed * value * (-1), 0, 0);
+			break;
+		    case "Right":
+			moveVector = new Vector3f(speed * value, 0, 0);
+			break;
+		    case "Forward":
+			moveVector = new Vector3f(0, 0, speed * value * (-1));
+			break;
+		    case "Backward":
+			moveVector = new Vector3f(0, 0, speed * value);
+			break;
+		}
+		coordinateAxesNode.move(moveVector);
+		rotationOrbNode.move(moveVector);
+		spatial.move(moveVector);
+		rotationOrbNode.rotate(rotationQuaternion);
+		spatial.setLocalRotation(rotationQuaternion.mult(spatial.getLocalRotation()));
+	    }
+	}
+    };
+
+    @Override
+    protected void controlUpdate(float tpf) {
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+    }
+
+    /**
+     * Calculates the intersection of the plain with basis vector planeStart and
+     * normal vector planeNormal with the straight line with basis vector
+     * rayStart and direction vector rayDirection. For "Axis x" only the x-value
+     * is set in the vector, equivalent for y and z.
+     *
+     * @param planeStart plain basis vector
+     * @param planeNormal plan normal vector
+     * @param rayStart straight line basis vector
+     * @param rayDirection straight line direction vector
+     * @param axis "x Axsis", "y Axsis", "z Axsis" for only one axis value in
+     * the returned vector. For other values all three dimensions are set.
+     * @return
+     */
+    public static Vector3f getIntersectionWithPlane(Vector3f planeStart, Vector3f planeNormal, Vector3f rayStart, Vector3f rayDirection, String axis) {
+	float t = ((planeStart.subtract(rayStart)).dot(planeNormal) / rayDirection.dot(planeNormal));
+	Vector3f intersect = rayStart.add(rayDirection.mult(t));
+	if (axis == null) {
+	    axis = "";
+	}
+	switch (axis) {
+	    case "x Axis":
+		intersect.setY(0);
+		intersect.setZ(0);
+		break;
+	    case "y Axis":
+		intersect.setX(0);
+		intersect.setZ(0);
+		break;
+	    case "z Axis":
+		intersect.setX(0);
+		intersect.setY(0);
+		break;
+	}
+	return intersect;
+    }
+}
