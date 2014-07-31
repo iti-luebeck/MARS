d4db056d6b9f4e5d878814309ff700bec6f0b956
Tosik <tosik@iti.uni-luebeck.de>
2014-07-31 13:16:28 +0200

    - last icon cleanup - added help and about dialog

---
+++ MARS_NB/MARS/MARSCore/src/mars/core/AboutDialogAction.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mars.core;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Help",
+        id = "mars.core.AboutDialogAction")
+@ActionRegistration(
+        iconBase = "mars/core/help.png",
+        displayName = "#CTL_AboutDialogAction")
+@ActionReference(path = "Menu/Help", position = 1400, separatorBefore = 1350)
+@Messages("CTL_AboutDialogAction=About")
+public final class AboutDialogAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        AboutPanel aboutPanel = new AboutPanel();
+        DialogDescriptor d = new DialogDescriptor(aboutPanel, "");
+        DialogDisplayer.getDefault().notify(d);
+    }
+}
