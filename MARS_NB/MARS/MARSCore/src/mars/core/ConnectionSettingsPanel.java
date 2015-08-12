9088f99bb5a0cc9a2f969f1071a2e0307511b656
fab <dev@planetfab.de>
2015-08-12 11:20:00 +0200

    auv connection settings window (WIP)

--- MARS_NB/MARS/MARSCore/src/mars/core/ConnectionSettingsPanel.java
+++ MARS_NB/MARS/MARSCore/src/mars/core/ConnectionSettingsPanel.java
@@ -5,10 +5,19 @@
  */
 package mars.core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import mars.MARS_Main;
+import mars.auv.AUV;
+import mars.auv.AUV_Manager;
+import mars.communication.AUVConnectionType;
 import net.miginfocom.swing.MigLayout;
 
 /**
@@ -23,10 +32,10 @@ public class ConnectionSettingsPanel extends JPanel {
 
     public void initialize(MARS_Main mars) {
 
-        JLabel auvHeadline = new JLabel("AUV");
-        JLabel connectionTypeHeadline = new JLabel("Connection Type");
-        JLabel hostconfigHeadline = new JLabel("Host Config");
-        JLabel statusHeadline = new JLabel("Status");
+        JLabel auvHeadline = new JLabel("<html><b>AUV</b></html>");
+        JLabel connectionTypeHeadline = new JLabel("<html><b>Connector</b></html>");
+        JLabel hostconfigHeadline = new JLabel("<html><b>Host Config</b></html>");
+        JLabel statusHeadline = new JLabel("<html><b>Status</b></html>");
 
         JPanel settingsContainer = new JPanel();
         settingsContainer.setLayout(new MigLayout("fill"));
@@ -38,12 +47,58 @@ public class ConnectionSettingsPanel extends JPanel {
         settingsContainer.add(hostconfigHeadline, "cell 2 0");
         settingsContainer.add(statusHeadline, "cell 3 0");
 
-        addAUVs(mars);
+        addAUVs(mars, settingsContainer);
 
         this.add(settingsContainer, "cell 0 0, grow");
     }
 
-    private void addAUVs(MARS_Main mars) {
-        //TODO
+    private void addAUVs(MARS_Main mars, JPanel settingsContainer) {
+        AUV_Manager auvManager = mars.getMapstate().getAUV_Manager();
+
+        if (auvManager == null) {
+            return;
+        }
+
+        HashMap<String, AUV> marsObjects = auvManager.getMARSObjects();
+
+        int row = 1;
+
+        for (String auvName : marsObjects.keySet()) {
+            AUV auv = marsObjects.get(auvName);
+
+            JLabel name = new JLabel(auv.getName());
+            final JComboBox combobox = new JComboBox(AUVConnectionType.values());
+
+            // set the right value
+            if (auv != null && auv.getAuvConnection() != null) {
+                combobox.setSelectedItem(auv.getAuvConnection().getConnectionType());
+            }
+
+            final JTextField hostconfig = new JTextField("127.0.0.1:8080");
+            hostconfig.setSize(150, 10);
+            if (combobox.getSelectedItem() != AUVConnectionType.TCP) {
+                hostconfig.setEnabled(false);
+            }
+
+            combobox.addActionListener(new ActionListener() {
+
+                public void actionPerformed(ActionEvent e) {
+                    hostconfig.setEnabled(combobox.getSelectedItem() == AUVConnectionType.TCP);
+                }
+            });
+
+            JLabel status = new JLabel();
+            status.setIcon(new javax.swing.ImageIcon(getClass().getResource("/mars/gui/resources/icons/reddot.png")));
+
+            JButton connectButton = new JButton("Connect");
+
+            settingsContainer.add(name, "cell 0 " + row);
+            settingsContainer.add(combobox, "cell 1 " + row);
+            settingsContainer.add(hostconfig, "cell 2 " + row + ", align left, growx, wmin 200");
+            settingsContainer.add(status, "cell 3 " + row + ", align center");
+            settingsContainer.add(connectButton, "cell 4 " + row);
+
+            row++;
+        }
     }
 }
